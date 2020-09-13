2678943c0024a25af1deccc30c58b578a29ff542
hj <jjjjhjjjj123@gmail.com>
2020-09-14 00:33:45 +0900

    Updates

---
+++ precache-manifest.bbd7e902e997dbd2ea1874b39ab422a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47f83e5b21bdfecc978a694e9f49f13",
+    "url": "/jjjjhjjjj.github.io/Todo-App/index.html"
+  },
+  {
+    "revision": "9b1c5629973d818ef46b",
+    "url": "/jjjjhjjjj.github.io/Todo-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b4fdf0f6c4736fb1b3fb",
+    "url": "/jjjjhjjjj.github.io/Todo-App/static/js/2.49aa4735.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jjjjhjjjj.github.io/Todo-App/static/js/2.49aa4735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1c5629973d818ef46b",
+    "url": "/jjjjhjjjj.github.io/Todo-App/static/js/main.28ab287e.chunk.js"
+  },
+  {
+    "revision": "5cf6249fd8580ea0ee4f",
+    "url": "/jjjjhjjjj.github.io/Todo-App/static/js/runtime-main.8fa9ff71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jjjjhjjjj.github.io/Todo-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
